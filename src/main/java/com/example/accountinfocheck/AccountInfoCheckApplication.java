c6872159e1932cc1e0a99631530698a1c5aa0a10
홍서정 <94052799+hsj517@users.noreply.github.com>
2023-08-15 23:08:20 +0900

    init

---
+++ src/main/java/com/example/accountinfocheck/AccountInfoCheckApplication.java
@@ -0,0 +1,13 @@
+package com.example.accountinfocheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountInfoCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountInfoCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/accountinfocheck/AccountInfoCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accountinfocheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountInfoCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
