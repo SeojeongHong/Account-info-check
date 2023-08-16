aedb27cda9e5977c9408a7a2012dbeeb3636c067
홍서정 <94052799+hsj517@users.noreply.github.com>
2023-08-16 23:18:00 +0900

    feat : 조회 결과 메세지 출력

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,14 +1,28 @@
 //조회 API 호출
 const searchButton = document.getElementById("searchButton");
 const accountInput = document.getElementById("accountInput");
-const resultDiv = document.getElementById("result");
 
+const count = document.getElementById("count");
+
+const msg = document.getElementById("msg");
+const safeMsg = document.getElementById("safe-msg");
+const unsafeMsg = document.getElementById("unsafe-msg");
 searchButton.addEventListener("click", () => {
     const account = accountInput.value;
     if (account) {
         const apiUrl = "/cheat/" + account;
         $.get(apiUrl, function (data) {
-            resultDiv.textContent = data + "건의 신고가 접수 되었습니다.";
+            if(data>0) {
+                count.textContent = data;
+                msg.style.display = "none";
+                safeMsg.style.display = "none";
+                unsafeMsg.style.display = "block";
+            }
+            else {
+                msg.style.display = "none";
+                unsafeMsg.style.display = "none";
+                safeMsg.style.display = "block";
+            }
         });
     }
 });
